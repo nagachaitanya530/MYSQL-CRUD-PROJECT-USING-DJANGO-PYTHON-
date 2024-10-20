e9c10e2441d9e7567ad1f787026832d3bf65d68d
Naga Chaitanya <nagachaitanya530@gmail.com>
2024-10-20 12:27:10 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render,redirect
+from crudapp.models import Student
+
+def index(request):
+    return render(request,'index.html')
+
+def save(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        mail = request.POST.get('mail')
+        age = request.POST.get('age')
+        Student.objects.create(name=name,address=address,mail=mail,age=age)
+        return redirect('display')
+    else:
+        return render('display')
+
+def display(request):
+    students = Student.objects.all()
+    return render(request,'display.html',{'students':students})
+
+def delete(request,pk):
+    student = Student.objects.get(id=pk)
+    student.delete()
+    return redirect('display')
+
+def update(request,pk):
+    student = Student.objects.get(id=pk)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        mail = request.POST.get('mail')
+        age = request.POST.get('age')
+        student.name = name
+        student.address = address
+        student.mail = mail
+        student.age = age
+        student.save()
+        return redirect('display')
+    else:
+        return render(request,'update.html',{'student':student})
